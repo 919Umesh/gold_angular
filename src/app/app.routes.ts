1a6c740733e9c99ea9c487d90c156ca084b94f4c
Umesh <globaltechumesh11@gmail.com>
2025-11-19 11:29:37 +0545

    Secure Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,24 @@ import { About } from './about/about';
 import { Profile } from './profile/profile';
 import { Transaction } from './transaction/transaction';
 import { Login } from './login/login';
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {path:'',component:Home},
     { path: 'home', component: Home },
     { path: 'login', component: Login },
     { path: 'about', component: About },
-    { path: 'profile', component: Profile },
-    { path: 'transactions', component: Transaction },
+    { 
+    path: 'profile', 
+    component: Profile,
+    canActivate: [AuthGuard] 
+    },
+   // { path: 'profile', component: Profile },
+   // { path: 'transactions', component: Transaction },
+   { 
+    path: 'transactions', 
+    component: Transaction,
+    canActivate: [AuthGuard]  // Protected route
+  },
     { path: '**', redirectTo: '' } 
 ];
